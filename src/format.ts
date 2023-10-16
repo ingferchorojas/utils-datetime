e0a1542455fe4efe75f779c8f4620540b6bb6089
Fernando Rojas <frojas@monchis.com.py>
2023-10-16 11:33:52 -0300

    fix: shortFormat

--- src/format.ts
+++ src/format.ts
@@ -4,7 +4,7 @@ const shortFormat = (date: Date, template: string): string => {
     const fullYear = date.getUTCFullYear()
   
     // Generar el objeto con las partes de la fecha
-    formattedDate.year = fullYear;
+    formattedDate.year = fullYear
     formattedDate.month = date.getUTCMonth() + 1
     formattedDate.day = date.getUTCDate()
     formattedDate.hour = date.getUTCHours()
@@ -12,15 +12,25 @@ const shortFormat = (date: Date, template: string): string => {
     formattedDate.second = date.getUTCSeconds()
     formattedDate.milliseconds = date.toISOString().split('.')[1].slice(0, -1)
   
+    // Determinar si es AM o PM
+    const isPM = formattedDate.hour >= 12
+    const amPm = isPM ? 'PM' : 'AM'
+    const hour12 = formattedDate.hour % 12 || 12 // Convertir 0 a 12 para las 12 AM
+  
     // Template personalizado para el formato deseado
-    return template.replace(/YYYY/g, formattedDate.year.toString())
-                   .replace(/YY/g, (formattedDate.year % 100).toString().padStart(2, '0'))
-                   .replace(/MM/g, String(formattedDate.month).padStart(2, '0'))
-                   .replace(/DD/g, String(formattedDate.day).padStart(2, '0'))
-                   .replace(/HH/g, String(formattedDate.hour).padStart(2, '0'))
-                   .replace(/MI/g, String(formattedDate.minute).padStart(2, '0'))
-                   .replace(/SS/g, String(formattedDate.second).padStart(2, '0'))
-                   .replace(/MS/g, String(formattedDate.milliseconds).padStart(3, '0'))
+    const formattedString = template
+      .replace(/YYYY/g, formattedDate.year.toString())
+      .replace(/YY/g, (formattedDate.year % 100).toString().padStart(2, '0'))
+      .replace(/MM/g, String(formattedDate.month).padStart(2, '0'))
+      .replace(/DD/g, String(formattedDate.day).padStart(2, '0'))
+      .replace(/HH/g, String(formattedDate.hour).padStart(2, '0'))
+      .replace(/H/g, hour12.toString()) // 12 horas sin ceros iniciales
+      .replace(/MI/g, String(formattedDate.minute).padStart(2, '0'))
+      .replace(/SS/g, String(formattedDate.second).padStart(2, '0'))
+      .replace(/MS/g, String(formattedDate.milliseconds).padStart(3, '0'))
+      .replace(/A/g, amPm) // Reemplazar 'A' con AM/PM
+  
+    return formattedString
 }
 
 export {
